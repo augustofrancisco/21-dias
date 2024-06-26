b18a13c08760f81e63b3b30975a25d2142107ac6
Augusto Francisco <augusto.franciscoo@hotmail.com>
2024-06-26 17:19:33 -0300

    melhorando o código

--- dia-03/dia03.js
+++ dia-03/dia03.js
@@ -15,9 +15,13 @@ let altura = Number(prompt("digite a sua altura"))
 
 let peso = Number(prompt("digite o seu peso"))
 
-let anoNasc = Number(2024 - idade)
+let anoNasc = Number(2024 - idade) 
 
-let imc = parseInt(peso / (altura * altura))
+let imc = peso / (altura * altura)
 
-console.log("Olá " + nome + ", você tem " + idade + " anos, nasceu no ano de " + anoNasc + ", tem " + altura + " de altura, pesa " + peso + "Kg e tem um IMC de " + imc + " Kg/m²")
+console.log(`Olá ${nome}, você tem ${idade} anos, nasceu em ${anoNasc}, tem ${altura} de altura, pesa ${peso}kg e seu IMC é de ${imc.toFixed(2)}kg/m²`) 
+
+/* let n1 = 3.14159
+
+console.log(n1.toFixed(3)) */
 
